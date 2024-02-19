fd357644c7903a3058f19a85ae6ac673188ab094
zaid03 <terguyzaid@gmail.com>
2024-02-19 11:39:52 +0100

    pls word v2

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -11,21 +11,38 @@ import requests
 import sys
 
 
-if len(sys.argv) < 2:
-    print("Usage: python script.py <employee_id>")
-    sys.exit(1)
+# Define the base URL for the API requests
+base_url = "https://jsonplaceholder.typicode.com/"
 
-employee_id = sys.argv[1]
-response = requests.get(base_url + "users/{}".format(employee_id))
+def get_employee_todo_progress(employee_id):
+    # Fetch user data
+    user_response = requests.get(base_url + "users/{}".format(employee_id))
+    if user_response.status_code != 200:
+        print("Failed to fetch user data. Status code:", user_response.status_code)
+        return
 
-if response.status_code == 200:
-    user_data = response.json()
-    employee_name = user_data.get('name', '')
+    user_data = user_response.json()
+    employee_name = user_data.get('name', 'Unknown')
 
-    # Check if the length of the employee name is 18 characters
-    if len(employee_name) == 18:
-        print("Employee Name: OK")
-    else:
-        print("Employee Name: Incorrect")
-else:
-    print("Failed to fetch user data. Status code:", response.status_code)
+    # Fetch user's todo list
+    todo_response = requests.get(base_url + "todos?userId={}".format(employee_id))
+    if todo_response.status_code != 200:
+        print("Failed to fetch TODO list data. Status code:", todo_response.status_code)
+        return
+
+    todo_data = todo_response.json()
+    total_tasks = len(todo_data)
+    completed_tasks = [task for task in todo_data if task['completed']]
+
+    # Print employee's todo list progress
+    print("Employee {} is done with tasks({}/{}):".format(employee_name, len(completed_tasks), total_tasks))
+    for task in completed_tasks:
+        print("\t", task['title'])
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
+        print("Usage: python script.py <employee_id>")
+        sys.exit(1)
+
+    employee_id = int(sys.argv[1])
+    get_employee_todo_progress(employee_id)
